14515eb356057725afffe2179203639825ef386c
unknown <mengheak088@gmail.com>
2025-08-10 13:24:05 +0700

    Dynamic route fixing

--- app/meals/[mealsId]/page.tsx
+++ app/meals/[mealsId]/page.tsx
@@ -1,7 +1,5 @@
-export default async function MealsDetailsPage({
-  params,
-}: {
-  params: { mealsId: string };
-}) {
+type MealsPageProps = { params: { mealsId: string } };
+
+export default async function MealsDetailsPage({ params }: MealsPageProps) {
   return <main>This is {params.mealsId} page</main>;
 }
